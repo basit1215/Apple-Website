013ca612179deb8700d91e8d6bce761bdded22eb
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-08-31 05:15:16 +0500

    updated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <>
+    <div>
+    <h1>Hello World!</h1>
+    <NavBar href="#" name="Store"/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+<img src alt />
+    <a href="#">Store</a>
+    <a href="#">MAC</a>
+    <a href="#">IOS</a>
+    <a href="#">Store</a>
+    <a href="#">Store</a>
+    <a href="#">Store</a>
+    
+    </>
+  )
+}
+
+export default NavBar
